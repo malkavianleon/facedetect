de6a10f3636dd888bd2694466b51d7f27775acc4
Leon Gonçalves <69921898+malkavianleon@users.noreply.github.com>
2022-03-11 17:11:55 -0300

    Arquivos Prontos depois do Particles
    Arquivos Depois do Particles funcionando normalmente, 
    
    Estou salvando esse backup, pois quando comecei a trabalhar com a API, tive problemas para dar o comando NPM START

--- src/App.js
+++ src/App.js
@@ -4,11 +4,90 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-tsparticles';
+
+const particlesOptions = {
+  
+}
 
 class App extends Component{
   render() {
     return (
     <div className="App">
+      <Particles className='particles' id="tsparticles"
+      options={{
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 200,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1000,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 3,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
       <Navigation />
       <Logo />
       <Rank />
