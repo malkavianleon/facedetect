4933a0e559c830f0236b45af4ea2af8a2c5a4264
Leon Gonçalves <69921898+malkavianleon@users.noreply.github.com>
2022-03-14 18:09:09 -0300

    Adicionado função de detecção de rostos

--- src/App.js
+++ src/App.js
@@ -90,9 +90,29 @@ class App extends Component{
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
@@ -104,13 +124,10 @@ class App extends Component{
     .predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-   .then((response) => {
-    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-   })
-   .catch((err) => {
-    console.log(err);
-   });
-   };   
+   .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+   .catch(err => console.log(err));
+   };
+      
 
   render() {
     return (
@@ -122,7 +139,7 @@ class App extends Component{
       <Logo />
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <FaceRecongnition imageUrl={this.state.imageUrl} /> 
+      <FaceRecongnition box={this.state.box} imageUrl={this.state.imageUrl} /> 
     </div>
     );
    }
